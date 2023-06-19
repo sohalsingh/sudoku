d605a022502418dcee21b8d426a24ee7c5345c35
Sohal Singh <singhsohal575@gmail.com>
2023-06-19 12:21:08 +0530

    first commit

---
+++ sudoku.py
@@ -0,0 +1,85 @@
+def print_board(board):
+    print("-------------------------")
+    for i in range(9):
+        if i % 3 == 0 and i != 0:
+            print("- - - - - - - - - - - - -")
+        for j in range(9):
+            if j % 3 == 0 and j != 0:
+                print(" | ", end="")
+            if j == 8:
+                print(board[i][j])
+            else:
+                print(str(board[i][j]) + " ", end="")
+    print("-------------------------")
+
+def find_empty_cell(board):
+    for i in range(9):
+        for j in range(9):
+            if board[i][j] == 0:
+                return i, j
+    return None
+
+def is_valid_move(board, num, row, col):
+    # Check row
+    for j in range(9):
+        if board[row][j] == num and j != col:
+            return False
+
+    # Check column
+    for i in range(9):
+        if board[i][col] == num and i != row:
+            return False
+
+    # Check 3x3 box
+    box_row = (row // 3) * 3
+    box_col = (col // 3) * 3
+    for i in range(box_row, box_row + 3):
+        for j in range(box_col, box_col + 3):
+            if board[i][j] == num and (i, j) != (row, col):
+                return False
+
+    return True
+
+def solve_board(board):
+    empty_cell = find_empty_cell(board)
+    if not empty_cell:
+        return True
+
+    row, col = empty_cell
+
+    for num in range(1, 10):
+        if is_valid_move(board, num, row, col):
+            board[row][col] = num
+
+            if solve_board(board):
+                return True
+
+            board[row][col] = 0
+
+    return False
+
+def play_game():
+    print("Welcome to Sudoku!")
+
+    board = [
+        [5, 3, 0, 0, 7, 0, 0, 0, 0],
+        [6, 0, 0, 1, 9, 5, 0, 0, 0],
+        [0, 9, 8, 0, 0, 0, 0, 6, 0],
+        [8, 0, 0, 0, 6, 0, 0, 0, 3],
+        [4, 0, 0, 8, 0, 3, 0, 0, 1],
+        [7, 0, 0, 0, 2, 0, 0, 0, 6],
+        [0, 6, 0, 0, 0, 0, 2, 8, 0],
+        [0, 0, 0, 4, 1, 9, 0, 0, 5],
+        [0, 0, 0, 0, 8, 0, 0, 7, 9]
+    ]
+
+    print("Here's the initial board:")
+    print_board(board)
+
+    if solve_board(board):
+        print("Solution:")
+        print_board(board)
+    else:
+        print("No solution exists for the given board.")
+
+play_game()
